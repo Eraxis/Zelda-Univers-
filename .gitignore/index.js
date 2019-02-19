5a9445ed27e65eff8988373f3d7f2408049710ad
Eraxis <47780957+Eraxis@users.noreply.github.com>
2019-02-19 13:41:29 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
-var bot = new Discord.Client();
+const bot = new Discord.Client();
 
 
-bot.on('ready', function() {
-    bot.user.setGame("Zelda Univers, https://discord.gg/gJdAfu6"); 
+bot.on('ready', () => {
+    bot.user.setGame("Zelda Univers, https://discord.gg/gJdAfu6");
     console.log("Bot prêt !");
 });
 
-bot.login('NTQ3MTc4NDI2NzM0NDc3MzQy.D0y_hw.rQ1cy_gCQT3wI6U5U6WYpJe7zmk');
+bot.login('NTQ3MTc4NDI2NzM0NDc3MzQy.D02KuA.sjSAGojTerYpuw99fFRfM__HCQ8');
